39d14ca83461534eb82c028e10ee7ca05512d205
Curtis Shmyr <cjshmyr@gmail.com>
2020-05-04 19:53:53 -0600

    Added a -Package command for build-maps.ps1

--- build-maps.ps1
+++ build-maps.ps1
@@ -3,6 +3,7 @@
 
 # This script builds the Renegade 2D maps.
 # Providing the "-Deploy" argument will build the map in the appropriate OpenRA support folder, instead of a bin folder. Currently supported on Windows only.
+# Providing the "-Package" argument will zip the map in a .oramap file, and remove the loose folder version when done.
 # Providing the "-NoRuleCopy" argument will ignore copying over yaml rules and lua; useful when making layout changes in the map editor.
 
 # In summary it:
@@ -14,22 +15,36 @@
 [CmdLetBinding()]
 param(
     [switch]$Deploy,
+    [switch]$Package,
     [switch]$NoRuleCopy
 )
 
 # The OpenRA engine version is set as a script variable, which should get updated when targeting new releases.
 $openraVersion = 'release-20200503'
 
+# This is a hack to flatten where we place our files, since OpenRA does not understand directories in .oramap files.
+$flattenAtTarget = $true
+
 # Returns yaml custom rule strings.
 function Get-MapYamlRulePaths($mod)
 {
     $modYaml = "$($PSScriptRoot)\mods\$($mod)\yaml\"
 
-    # Hacky; instead of this, loop through subfolders and append appropriately.
-    $yaml = "`nRules: " + (((Get-ChildItem "$($modYaml)\rules") | ForEach-Object { "yaml\$($mod)\rules\$($_.Name)" }) -join ', ')
-    $yaml += "`nWeapons: " + (((Get-ChildItem "$($modYaml)\weapons") | ForEach-Object { "yaml\$($mod)\weapons\$($_.Name)" }) -join ', ')
-    $yaml += "`nNotifications: " + (((Get-ChildItem "$($modYaml)\notifications") | ForEach-Object { "yaml\$($mod)\notifications\$($_.Name)" }) -join ', ')
-    $yaml += "`nSequences: " + (((Get-ChildItem "$($modYaml)\sequences") | ForEach-Object { "yaml\$($mod)\sequences\$($_.Name)" }) -join ', ')
+    if (!$flattenAtTarget)
+    {
+        $yaml = "`nRules: " + (((Get-ChildItem "$($modYaml)\rules") | ForEach-Object { "yaml\$($mod)\rules\$($_.Name)" }) -join ', ')
+        $yaml += "`nWeapons: " + (((Get-ChildItem "$($modYaml)\weapons") | ForEach-Object { "yaml\$($mod)\weapons\$($_.Name)" }) -join ', ')
+        $yaml += "`nNotifications: " + (((Get-ChildItem "$($modYaml)\notifications") | ForEach-Object { "yaml\$($mod)\notifications\$($_.Name)" }) -join ', ')
+        $yaml += "`nSequences: " + (((Get-ChildItem "$($modYaml)\sequences") | ForEach-Object { "yaml\$($mod)\sequences\$($_.Name)" }) -join ', ')
+    }
+    else
+    {
+        # Hack.
+        $yaml = "`nRules: " + (((Get-ChildItem "$($modYaml)\rules") | ForEach-Object { "$($_.Name)" }) -join ', ')
+        $yaml += "`nWeapons: " + (((Get-ChildItem "$($modYaml)\weapons") | ForEach-Object { "$($_.Name)" }) -join ', ')
+        $yaml += "`nNotifications: " + (((Get-ChildItem "$($modYaml)\notifications") | ForEach-Object { "$($_.Name)" }) -join ', ')
+        $yaml += "`nSequences: " + (((Get-ChildItem "$($modYaml)\sequences") | ForEach-Object { "$($_.Name)" }) -join ', ')
+    }
 
     return $yaml
 }
@@ -63,8 +78,8 @@ foreach ($mod in Get-ChildItem "$($PSScriptRoot)\mods\")
     foreach ($mapFolder in $mapFolders)
     {
         # Delete map if exists.
-        $existingMapFolder = "$($destination)\$($mapFolder)"
-        Remove-Item $existingMapFolder -Recurse -ErrorAction Ignore
+        $targetMapFolder = "$($destination)\$($mapFolder)"
+        Remove-Item $targetMapFolder -Recurse -ErrorAction Ignore
 
         # Copy map.
         $mapFromPath = "$($modMapsFolder)\$($mapFolder)"
@@ -72,8 +87,20 @@ foreach ($mod in Get-ChildItem "$($PSScriptRoot)\mods\")
 
         if (!$NoRuleCopy) {
             # Copy rules.
-            $rulesToPath = "$($destination)\$($mapFolder)\yaml\$($mod)"
-            Copy-Item -Path $modRulesFolder -Destination $rulesToPath -Recurse -Force
+            if (!$flattenAtTarget)
+            {
+                $rulesToPath = "$($destination)\$($mapFolder)\yaml\$($mod)"
+                Copy-Item -Path $modRulesFolder -Destination $rulesToPath -Recurse -Force
+            }
+            else
+            {
+                # Hack.
+                $rulesToPath = "$($destination)\$($mapFolder)"
+                Copy-Item -Path (Get-ChildItem -Path "$($modRulesFolder)\notifications").FullName -Destination $rulesToPath -Recurse -Force
+                Copy-Item -Path (Get-ChildItem -Path "$($modRulesFolder)\rules").FullName -Destination $rulesToPath -Recurse -Force
+                Copy-Item -Path (Get-ChildItem -Path "$($modRulesFolder)\sequences").FullName -Destination $rulesToPath -Recurse -Force
+                Copy-Item -Path (Get-ChildItem -Path "$($modRulesFolder)\weapons").FullName -Destination $rulesToPath -Recurse -Force
+            }
 
             # Append any custom rules to map.yaml.
             $mapDotYamlPath = "$($destination)\$($mapFolder)\map.yaml"
@@ -81,8 +108,33 @@ foreach ($mod in Get-ChildItem "$($PSScriptRoot)\mods\")
             Add-Content -Path $mapDotYamlPath -Value $mapDotYamlRulePaths
 
             # Copy lua script(s).
-            $scriptToPath = "$($destination)\$($mapFolder)\lua"
-            Copy-Item -Path $luaFolder -Destination $scriptToPath -Recurse -Force
+            if (!$flattenAtTarget)
+            {
+                $scriptToPath = "$($destination)\$($mapFolder)\lua"
+                Copy-Item -Path $luaFolder -Destination $scriptToPath -Recurse -Force
+            }
+            else
+            {
+                # Hack.
+                $scriptToPath = "$($destination)\$($mapFolder)"
+                Copy-Item -Path (Get-ChildItem -Path $luaFolder).Fullname -Destination $scriptToPath -Recurse -Force
+            }
+        }
+
+        # If creating a packaged .oramap file
+        if ($Package)
+        {
+            # Delete package if exists.
+            Remove-Item "$($targetMapFolder).oramap" -Recurse -ErrorAction Ignore
+
+            # Create the zip file
+            Compress-Archive -Path (Get-ChildItem -Path $targetMapFolder).FullName -DestinationPath "$($targetMapFolder).zip"
+
+            # Rename to .oramap
+            Rename-Item -Path "$($targetMapFolder).zip" -NewName "$($targetMapFolder).oramap"
+
+            # Remove the target directory.
+            Remove-Item $targetMapFolder -Recurse -ErrorAction Ignore
         }
     }
 }
