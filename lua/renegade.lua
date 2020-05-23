6d08cd345f5eea48247d30da623563a47a286274
Curtis Shmyr <cjshmyr@gmail.com>
2020-05-23 11:26:13 -0600

    Update version to 1.01

--- lua/renegade.lua
+++ lua/renegade.lua
@@ -1,6 +1,6 @@
 --[[
 	Renegade 2D: Lua script by @hamb
-	Version: 1.00
+	Version: 1.01
 	Engine: OpenRA release-20200504
 ]]
 
@@ -111,7 +111,7 @@ NeutralPlayer = Player.GetPlayer(NeutralPlayerName)
 WorldLoaded = function()
 	Media.PlaySound(SoundMissionStarted)
 
-	DisplayMessage('Welcome to Renegade 2D (version 1.00)!')
+	DisplayMessage('Welcome to Renegade 2D (version 1.01)!')
 
 	SetPlayerInfo()
 	SetTeamInfo()
