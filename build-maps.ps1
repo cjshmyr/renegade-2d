c025b3cc92721a78a11f795c26d840669860fef9
Curtis Shmyr <cjshmyr@gmail.com>
2020-06-11 19:12:54 -0600

    Update build-maps.ps1 to use folders again

--- build-maps.ps1
+++ build-maps.ps1
@@ -22,29 +22,15 @@ param(
 # The OpenRA engine version is set as a script variable, which should get updated when targeting new releases.
 $openraVersion = 'release-20200503'
 
-# This is a hack to flatten where we place our files, since OpenRA does not understand directories in .oramap files.
-$flattenAtTarget = $true
-
 # Returns yaml custom rule strings.
 function Get-MapYamlRulePaths($mod)
 {
     $modYaml = "$($PSScriptRoot)\mods\$($mod)\yaml\"
 
-    if (!$flattenAtTarget)
-    {
-        $yaml = "`nRules: " + (((Get-ChildItem "$($modYaml)\rules") | ForEach-Object { "yaml\$($mod)\rules\$($_.Name)" }) -join ', ')
-        $yaml += "`nWeapons: " + (((Get-ChildItem "$($modYaml)\weapons") | ForEach-Object { "yaml\$($mod)\weapons\$($_.Name)" }) -join ', ')
-        $yaml += "`nNotifications: " + (((Get-ChildItem "$($modYaml)\notifications") | ForEach-Object { "yaml\$($mod)\notifications\$($_.Name)" }) -join ', ')
-        $yaml += "`nSequences: " + (((Get-ChildItem "$($modYaml)\sequences") | ForEach-Object { "yaml\$($mod)\sequences\$($_.Name)" }) -join ', ')
-    }
-    else
-    {
-        # Hack.
-        $yaml = "`nRules: " + (((Get-ChildItem "$($modYaml)\rules") | ForEach-Object { "$($_.Name)" }) -join ', ')
-        $yaml += "`nWeapons: " + (((Get-ChildItem "$($modYaml)\weapons") | ForEach-Object { "$($_.Name)" }) -join ', ')
-        $yaml += "`nNotifications: " + (((Get-ChildItem "$($modYaml)\notifications") | ForEach-Object { "$($_.Name)" }) -join ', ')
-        $yaml += "`nSequences: " + (((Get-ChildItem "$($modYaml)\sequences") | ForEach-Object { "$($_.Name)" }) -join ', ')
-    }
+    $yaml = "`nRules: " + (((Get-ChildItem "$($modYaml)\rules") | ForEach-Object { "yaml/$($mod)/rules/$($_.Name)" }) -join ', ')
+    $yaml += "`nWeapons: " + (((Get-ChildItem "$($modYaml)\weapons") | ForEach-Object { "yaml/$($mod)/weapons/$($_.Name)" }) -join ', ')
+    $yaml += "`nNotifications: " + (((Get-ChildItem "$($modYaml)\notifications") | ForEach-Object { "yaml/$($mod)/notifications/$($_.Name)" }) -join ', ')
+    $yaml += "`nSequences: " + (((Get-ChildItem "$($modYaml)\sequences") | ForEach-Object { "yaml/$($mod)/sequences/$($_.Name)" }) -join ', ')
 
     return $yaml
 }
@@ -87,20 +73,8 @@ foreach ($mod in Get-ChildItem "$($PSScriptRoot)\mods\")
 
         if (!$NoRuleCopy) {
             # Copy rules.
-            if (!$flattenAtTarget)
-            {
-                $rulesToPath = "$($destination)\$($mapFolder)\yaml\$($mod)"
-                Copy-Item -Path $modRulesFolder -Destination $rulesToPath -Recurse -Force
-            }
-            else
-            {
-                # Hack.
-                $rulesToPath = "$($destination)\$($mapFolder)"
-                Copy-Item -Path (Get-ChildItem -Path "$($modRulesFolder)\notifications").FullName -Destination $rulesToPath -Recurse -Force
-                Copy-Item -Path (Get-ChildItem -Path "$($modRulesFolder)\rules").FullName -Destination $rulesToPath -Recurse -Force
-                Copy-Item -Path (Get-ChildItem -Path "$($modRulesFolder)\sequences").FullName -Destination $rulesToPath -Recurse -Force
-                Copy-Item -Path (Get-ChildItem -Path "$($modRulesFolder)\weapons").FullName -Destination $rulesToPath -Recurse -Force
-            }
+            $rulesToPath = "$($destination)\$($mapFolder)\yaml\$($mod)"
+            Copy-Item -Path $modRulesFolder -Destination $rulesToPath -Recurse -Force
 
             # Append any custom rules to map.yaml.
             $mapDotYamlPath = "$($destination)\$($mapFolder)\map.yaml"
@@ -108,18 +82,8 @@ foreach ($mod in Get-ChildItem "$($PSScriptRoot)\mods\")
             Add-Content -Path $mapDotYamlPath -Value $mapDotYamlRulePaths
 
             # Copy lua script(s).
-            $scriptToPath = ""
-            if (!$flattenAtTarget)
-            {
-                $scriptToPath = "$($destination)\$($mapFolder)\lua"
-                Copy-Item -Path $luaFolder -Destination $scriptToPath -Recurse -Force
-            }
-            else
-            {
-                # Hack.
-                $scriptToPath = "$($destination)\$($mapFolder)"
-                Copy-Item -Path (Get-ChildItem -Path $luaFolder).Fullname -Destination $scriptToPath -Recurse -Force
-            }
+            $scriptToPath = "$($destination)\$($mapFolder)\lua"
+            Copy-Item -Path $luaFolder -Destination $scriptToPath -Recurse -Force
 
             # Update mod reference in main lua script.
             $luaScriptFullPath = "$($scriptToPath)\renegade.lua"
